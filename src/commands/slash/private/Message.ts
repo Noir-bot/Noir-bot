d6144edcc2ef3cc125d615adf12e850d491802eb
Samvel Petrosyan <loidnoir@gmail.com>
2022-06-07 00:00:08 +0400

    🐢 Message command update
    - Message command improvements
    - Special message class improvements

--- src/commands/slash/private/Message.ts
+++ src/commands/slash/private/Message.ts
@@ -27,7 +27,7 @@ export default class MessageCommand extends NoirChatCommand {
     const id = `${interaction.user.id}${interaction.guild?.id}`
 
     if (!client.noirMessages.get(id)) {
-      client.noirMessages.set(id, new NoirMessage(id))
+      client.noirMessages.set(id, new NoirMessage(id, client, interaction))
     }
 
     await this.controlMessage(client, interaction, id)
@@ -40,15 +40,10 @@ export default class MessageCommand extends NoirChatCommand {
     const buttons = [
       [
         new ButtonBuilder().setCustomId(`message-${id}-content-button`).setLabel('Message content').setStyle(message?.content ? ButtonStyle.Success : ButtonStyle.Secondary),
-        new ButtonBuilder().setCustomId(`message-${id}-color-button`).setLabel('Embed color').setStyle(embed?.data.color ? ButtonStyle.Success : ButtonStyle.Secondary),
-        new ButtonBuilder().setCustomId(`message-${id}-image-button`).setLabel('Embed image').setStyle(embed?.data.image ? ButtonStyle.Success : ButtonStyle.Secondary),
-        new ButtonBuilder().setCustomId(`message-${id}-thumbnail-button`).setLabel('Embed thumbnail').setStyle(embed?.data.thumbnail ? ButtonStyle.Success : ButtonStyle.Secondary)
-      ],
-      [
-        new ButtonBuilder().setCustomId(`message-${id}-description-button`).setLabel('Embed description').setStyle(embed?.data.description ? ButtonStyle.Success : ButtonStyle.Secondary),
+        new ButtonBuilder().setCustomId(`message-${id}-settings-button`).setLabel('Embed').setStyle(message?.embedStatus ? ButtonStyle.Success : ButtonStyle.Secondary),
         new ButtonBuilder().setCustomId(`message-${id}-title-button`).setLabel('Embed title').setStyle(embed?.data.title ? ButtonStyle.Success : ButtonStyle.Secondary),
         new ButtonBuilder().setCustomId(`message-${id}-author-button`).setLabel('Embed author').setStyle(embed?.data.author?.name ? ButtonStyle.Success : ButtonStyle.Secondary),
-        new ButtonBuilder().setCustomId(`message-${id}-footer-button`).setLabel('Embed footer').setStyle(embed?.data.footer?.text ? ButtonStyle.Success : ButtonStyle.Secondary)
+        new ButtonBuilder().setCustomId(`message-${id}-footer-button`).setLabel('Embed footer').setStyle(embed?.data.footer?.text ? ButtonStyle.Success : ButtonStyle.Secondary),
       ],
       [
         new ButtonBuilder().setCustomId(`message-${id}-reset-button`).setLabel('Reset').setStyle(ButtonStyle.Danger),
@@ -61,7 +56,6 @@ export default class MessageCommand extends NoirChatCommand {
     const actionRows = [
       new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(buttons[0]),
       new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(buttons[1]),
-      new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(buttons[2]),
     ]
 
     await client.noirReply.reply({
@@ -93,20 +87,26 @@ export default class MessageCommand extends NoirChatCommand {
       return
     } else if (type == 'reset') {
       client.noirMessages.delete(id)
-      client.noirMessages.set(id, new NoirMessage(id))
+      client.noirMessages.set(id, new NoirMessage(id, client, interaction))
 
-      this.controlMessage(client, interaction, id)
+      await this.controlMessage(client, interaction, id)
     } else if (type == 'send') {
       const message = client.noirMessages.get(id)
       const embed = message?.embed.data
+      const status = message?.embedStatus
       const content = message?.content
 
       try {
-        if (embed && content) await interaction.channel?.send({ embeds: [embed], content: content })
-        else if (embed && !content) await interaction.channel?.send({ embeds: [embed] })
-        else if (!embed && content) await interaction.channel?.send({ content: content })
+        if (status && embed && content) await interaction.channel?.send({ embeds: [embed], content: content })
+        else if (status && embed && !content) await interaction.channel?.send({ embeds: [embed] })
+        else if (!status && content) {
+          await interaction.channel?.send({ content: content })
+          console.log(content)
+        }
+
+        this.controlMessage(client, interaction, id)
       } catch (err) {
-        client.noirReply.reply({
+        await client.noirReply.reply({
           interaction: interaction,
           author: 'Unexpected error',
           description: 'Can\'t send empty message',
@@ -114,6 +114,7 @@ export default class MessageCommand extends NoirChatCommand {
         })
       }
     } else if (type == 'content') await this.contentRequest(client, interaction, id)
+    else if (type == 'settings') await this.settingsRequest(client, interaction, id)
   }
 
   public async modalResponse(client: NoirClient, interaction: ModalSubmitInteraction) {
@@ -122,6 +123,7 @@ export default class MessageCommand extends NoirChatCommand {
     const id = parts[1]
 
     if (type == 'content') await this.contentResponse(client, interaction, id)
+    else if (type == 'settings') await this.settingsResponse(client, interaction, id)
   }
 
   public async contentRequest(client: NoirClient, interaction: ButtonInteraction, id: string) {
@@ -139,13 +141,87 @@ export default class MessageCommand extends NoirChatCommand {
     const modal = new ModalBuilder()
       .addComponents([contentActionRow])
       .setCustomId(`message-${id}-content-modal`)
-      .setTitle('Message constructor, content')
+      .setTitle('Message constructor')
     await interaction.showModal(modal)
   }
 
   public async contentResponse(client: NoirClient, interaction: ModalSubmitInteraction, id: string) {
     const content = interaction.fields.getTextInputValue(`message-${id}-content-input`)
     client.noirMessages.get(id)?.setContent(content)
+
+    await this.controlMessage(client, interaction, id)
+  }
+
+  public async settingsRequest(client: NoirClient, interaction: ButtonInteraction, id: string) {
+    const message = client.noirMessages.get(id)
+    const embed = message?.embed.data
+
+    const descriptionInput = new TextInputBuilder()
+      .setStyle(TextInputStyle.Paragraph)
+      .setCustomId(`message-${id}-description-input`)
+      .setLabel('Embed description')
+      .setValue(embed?.description ?? '')
+      .setPlaceholder('Enter embed description')
+      .setRequired(false)
+    const colorInput = new TextInputBuilder()
+      .setStyle(TextInputStyle.Short)
+      .setCustomId(`message-${id}-color-input`)
+      .setLabel('Embed color')
+      .setMaxLength(20)
+      .setValue(message?.color ?? '')
+      .setPlaceholder('Enter embed color (primary, secondary, tertiary, success, warning, embed)')
+      .setRequired(false)
+    const imageInput = new TextInputBuilder()
+      .setStyle(TextInputStyle.Short)
+      .setCustomId(`message-${id}-image-input`)
+      .setLabel('Embed image')
+      .setValue(message?.image ?? '')
+      .setPlaceholder('Enter embed image (server, user, client)')
+      .setRequired(false)
+    const thumbnailInput = new TextInputBuilder()
+      .setStyle(TextInputStyle.Short)
+      .setCustomId(`message-${id}-thumbnail-input`)
+      .setLabel('Embed thumbnail')
+      .setValue(message?.thumbnail ?? '')
+      .setPlaceholder('Enter embed thumbnail (server, user, client)')
+      .setRequired(false)
+
+    const contentActionRows = [
+      new ActionRowBuilder<ModalActionRowComponentBuilder>().addComponents([descriptionInput]),
+      new ActionRowBuilder<ModalActionRowComponentBuilder>().addComponents([colorInput]),
+      new ActionRowBuilder<ModalActionRowComponentBuilder>().addComponents([imageInput]),
+      new ActionRowBuilder<ModalActionRowComponentBuilder>().addComponents([thumbnailInput])
+    ]
+
+    const modal = new ModalBuilder()
+      .addComponents(contentActionRows)
+      .setCustomId(`message-${id}-settings-modal`)
+      .setTitle('Message constructor')
+    await interaction.showModal(modal)
+  }
+
+  public async settingsResponse(client: NoirClient, interaction: ModalSubmitInteraction, id: string) {
+    const description = interaction.fields.getTextInputValue(`message-${id}-description-input`) ?? undefined
+    const image = interaction.fields.getTextInputValue(`message-${id}-image-input`) ?? undefined
+    const thumbnail = interaction.fields.getTextInputValue(`message-${id}-thumbnail-input`) ?? undefined
+    const _color = interaction.fields.getTextInputValue(`message-${id}-color-input`) ?? undefined
+
+    if (_color) {
+      let color = 'secondary'
+
+      if (_color == 'primary') color = 'primary'
+      else if (_color == 'secondary') color = 'secondary'
+      else if (_color == 'tertiary') color = 'tertiary'
+      else if (_color == 'success') color = 'success'
+      else if (_color == 'warning') color = 'warning'
+      else if (_color == 'embed') color = 'embed'
+
+      client.noirMessages.get(id)?.setColor(color)
+    }
+
+    if (description) client.noirMessages.get(id)?.setDescription(description)
+    if (image) client.noirMessages.get(id)?.setImage(image)
+    if (thumbnail) client.noirMessages.get(id)?.setThumbnail(thumbnail)
 
     await this.controlMessage(client, interaction, id)
   }
--- src/libs/structures/Message.ts
+++ src/libs/structures/Message.ts
@@ -1,14 +1,19 @@
-import { ColorResolvable, EmbedBuilder, EmbedField } from 'discord.js'
+import { ColorResolvable, EmbedBuilder, EmbedField, Interaction } from 'discord.js'
 import { colors } from '../config/design'
 import { urlRegex } from '../config/patterns'
+import NoirClient from './Client'
 
 export default class NoirMessage {
   public _id: string
+  public client: NoirClient
+  public interaction: Interaction
   public properties: NoirMessageProperties
 
-  constructor(id: string) {
+  constructor(id: string, client: NoirClient, interaction: Interaction) {
     this._id = id
-    this.properties = { embed: { status: false } }
+    this.client = client
+    this.interaction = interaction
+    this.properties = { embed: { status: false, timestamp: false } }
   }
 
   public get id() {
@@ -21,13 +26,42 @@ export default class NoirMessage {
 
   public get embed() {
     const embed = new EmbedBuilder()
+    const clientAvatar = this.client.user?.avatarURL() ? this.client.user.avatarURL() : undefined
+    const userAvatar = this.interaction.user.avatarURL()
+    const serverIcon = this.interaction.guild?.iconURL() ? this.interaction.guild.iconURL() : undefined
 
     if (this.properties.embed.color) embed.setColor(this.properties.embed.color)
     if (this.properties.embed.description) embed.setDescription(this.properties.embed.description)
-    if (this.properties.embed.author) embed.setAuthor({ name: this.properties.embed.author, iconURL: this.properties.embed.authorImage })
-    if (this.properties.embed.footer) embed.setFooter({ text: this.properties.embed.footer, iconURL: this.properties.embed.footerImage })
-    if (this.properties.embed.image && urlRegex.test(this.properties.embed.image)) embed.setImage(this.properties.embed.image)
-    if (this.properties.embed.thumbnail && urlRegex.test(this.properties.embed.thumbnail)) embed.setThumbnail(this.properties.embed.thumbnail)
+    if (this.properties.embed.author) {
+      this.properties.embed.authorImageRaw = this.properties.embed.footerImage
+
+      if (this.properties.embed.authorImage == 'client' && userAvatar) embed.setAuthor({ name: this.properties.embed.author, iconURL: clientAvatar == null ? undefined : userAvatar })
+      else if (this.properties.embed.authorImage == 'user' && userAvatar) embed.setAuthor({ name: this.properties.embed.author, iconURL: userAvatar == null ? undefined : userAvatar })
+      else if (this.properties.embed.authorImage == 'server' && serverIcon) embed.setAuthor({ name: this.properties.embed.author, iconURL: serverIcon == null ? undefined : serverIcon })
+    }
+    if (this.properties.embed.footer) {
+      this.properties.embed.footerImageRaw = this.properties.embed.footerImage
+
+      if (this.properties.embed.footerImage == 'client' && userAvatar) embed.setFooter({ text: this.properties.embed.footer, iconURL: clientAvatar == null ? undefined : userAvatar })
+      else if (this.properties.embed.footerImage == 'user' && userAvatar) embed.setFooter({ text: this.properties.embed.footer, iconURL: userAvatar == null ? undefined : userAvatar })
+      else if (this.properties.embed.footerImage == 'server' && serverIcon) embed.setFooter({ text: this.properties.embed.footer, iconURL: serverIcon == null ? undefined : serverIcon })
+    }
+    if (this.properties.embed.image && urlRegex.test(this.properties.embed.image) || this.properties.embed.image == 'client' || this.properties.embed.image == 'user' || this.properties.embed.image == 'server') {
+      this.properties.embed.imageRaw = this.properties.embed.image
+
+      if (this.properties.embed.image == 'client' && clientAvatar) embed.setImage(clientAvatar)
+      else if (this.properties.embed.image == 'user' && userAvatar) embed.setImage(userAvatar)
+      else if (this.properties.embed.image == 'server' && serverIcon) embed.setImage(serverIcon)
+      else embed.setImage(this.properties.embed.image)
+    }
+    if (this.properties.embed.thumbnail && urlRegex.test(this.properties.embed.thumbnail) || this.properties.embed.thumbnail == 'client' || this.properties.embed.thumbnail == 'user' || this.properties.embed.thumbnail == 'server') {
+      this.properties.embed.thumbnailRaw = this.properties.embed.thumbnail
+
+      if (this.properties.embed.thumbnail == 'client' && userAvatar) embed.setThumbnail(userAvatar)
+      else if (this.properties.embed.thumbnail == 'user' && userAvatar) embed.setThumbnail(userAvatar)
+      else if (this.properties.embed.thumbnail == 'server' && serverIcon) embed.setThumbnail(serverIcon)
+      else embed.setThumbnail(this.properties.embed.thumbnail)
+    }
     if (this.properties.embed.title) {
       embed.setTitle(this.properties.embed.title)
       if (this.properties.embed.titleURL) embed.setURL(this.properties.embed.titleURL)
@@ -37,11 +71,35 @@ export default class NoirMessage {
       embed.addFields(this.properties.embed?.fields)
     }
 
-    if (this.properties?.content || this.properties.embed?.title || this.properties.embed?.author || this.properties.embed?.description || this.properties.embed?.fields) {
+    if (this.properties.embed?.title || this.properties.embed?.author || this.properties.embed?.description || this.properties.embed?.fields) {
       this.properties.embed.status = true
     }
 
     return embed
+  }
+
+  public get embedStatus() {
+    return this.properties.embed.status
+  }
+
+  public get color() {
+    return this.properties.embed.colorRaw
+  }
+
+  public get image() {
+    return this.properties.embed.imageRaw
+  }
+
+  public get thumbnail() {
+    return this.properties.embed.thumbnailRaw
+  }
+
+  public get authorImage() {
+    return this.properties.embed.authorImageRaw
+  }
+
+  public get footerImage() {
+    return this.properties.embed.footerImageRaw
   }
 
   public setContent(content: string) {
@@ -50,8 +108,15 @@ export default class NoirMessage {
     return this
   }
 
-  public setColor(color: colors) {
-    this.properties.embed.color = color
+  public setColor(color: string) {
+    this.properties.embed.colorRaw = color
+
+    if (color == 'primary') this.properties.embed.color = colors.Primary
+    else if (color == 'secondary') this.properties.embed.color = colors.Secondary
+    else if (color == 'tertiary') this.properties.embed.color = colors.Tertiary
+    else if (color == 'success') this.properties.embed.color = colors.Success
+    else if (color == 'warning') this.properties.embed.color = colors.Warning
+    else if (color == 'embed') this.properties.embed.color = colors.Embed
 
     return this
   }
@@ -91,7 +156,7 @@ export default class NoirMessage {
   }
 
   public setImage(image: string) {
-    if (urlRegex.test(image)) {
+    if (urlRegex.test(image) || image == 'client' || image == 'user' || image == 'server') {
       this.properties.embed.image = image
     }
 
@@ -99,7 +164,7 @@ export default class NoirMessage {
   }
 
   public setThumbnail(thumbnail: string) {
-    if (urlRegex.test(thumbnail)) {
+    if (urlRegex.test(thumbnail) || thumbnail == 'client' || thumbnail == 'user' || thumbnail == 'server') {
       this.properties.embed.thumbnail = thumbnail
     }
 
@@ -117,16 +182,22 @@ interface NoirMessageProperties {
   content?: string
   embed: {
     status: boolean
+    description?: string
     color?: ColorResolvable
+    colorRaw?: string
     title?: string
     titleURL?: string
     author?: string
     authorImage?: string
-    description?: string
+    authorImageRaw?: string
     footer?: string
     footerImage?: string
+    footerImageRaw?: string
     image?: string
+    imageRaw?: string
     thumbnail?: string
+    thumbnailRaw?: string
     fields?: EmbedField[]
+    timestamp: boolean
   }
 }
