ae18721253b264b5251925aa809f9b2c9205a080
Samvel Petrosyan <loidnoir@gmail.com>
2023-02-06 20:16:25 +0400

    refactor: config update

--- src/constants/Options.ts
+++ src/constants/Options.ts
@@ -4,9 +4,9 @@ import * as dotenv from 'dotenv'
 dotenv.config({ path: __dirname + '/../../.env' })
 
 export default class Options {
-  public static readonly token = process.env.ENVIRONMENT == 'DEVELOPMENT' ? process.env.DEV_TOKEN! : process.env.TOKEN!
-  public static readonly cluster = process.env.ENVIRONMENT == 'DEVELOPMENT' ? process.env.DEV_CLUSTER! : process.env.DATABASE!
-  public static readonly guildId = process.env.ENVIRONMENT == 'DEVELOPMENT' ? process.env.DEV_GUILD! : process.env.GUILD!
+  public static readonly token = process.env.TOKEN!
+  public static readonly cluster = process.env.DATABASE!
+  public static readonly guildId = process.env.GUILD!
   public static readonly errorChannelId = process.env.ERROR_CHANNEL_ID
   public static readonly loidAvatar = 'https://cdn.discordapp.com/avatars/690728155052245072/da5d06534afcc88e5cbdf50b15c6d487.png?size=4096'
   public static readonly clientAvatar = 'https://cdn.discordapp.com/avatars/905923544687464448/26900c0b67f2a6c047b356e966eb24d6.png?size=4096'
