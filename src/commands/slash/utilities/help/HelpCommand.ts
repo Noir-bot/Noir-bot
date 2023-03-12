b92cfcdc44d9abc76e06cf9bd77d401b0b32dd32
Samvel Petrosyan <loidnoir@gmail.com>
2023-03-12 19:26:12 +0400

    feat: documentation link in help command

--- src/commands/slash/utilities/help/HelpCommand.ts
+++ src/commands/slash/utilities/help/HelpCommand.ts
@@ -3,8 +3,8 @@ import Options from '@constants/Options'
 import Reply from '@helpers/Reply'
 import Client from '@structures/Client'
 import ChatCommand from '@structures/commands/ChatCommand'
+import { AccessType, CommandType } from '@structures/commands/Command'
 import { ActionRowBuilder, ApplicationCommandType, ButtonBuilder, ButtonInteraction, ButtonStyle, ChatInputCommandInteraction, MessageActionRowComponentBuilder } from 'discord.js'
-import {AccessType,CommandType} from '@structures/commands/Command'
 
 export default class HelpCommand extends ChatCommand {
   constructor(client: Client) {
@@ -32,9 +32,10 @@ export default class HelpCommand extends ChatCommand {
 
   public static async initialMessage(client: Client, interaction: ChatInputCommandInteraction | ButtonInteraction): Promise<void> {
     const button = new ButtonBuilder()
-      .setCustomId('help-faq')
-      .setLabel('F.A.Q.')
-      .setStyle(ButtonStyle.Secondary)
+      .setURL(Options.docsLink)
+      .setLabel('Documentation')
+      .setStyle(ButtonStyle.Link)
+
     const actionRow = new ActionRowBuilder<MessageActionRowComponentBuilder>()
       .addComponents(button)
 
@@ -46,39 +47,6 @@ export default class HelpCommand extends ChatCommand {
       authorImage: client.user?.avatarURL(),
       description: `Hey there, if you have any problems or questions contact our support team. Be sure to join [support server](${Options.guildInvite})`,
       components: [actionRow]
-    })
-  }
-
-  public static async faqMessage(client: Client, interaction: ButtonInteraction): Promise<void> {
-    const button = new ButtonBuilder()
-      .setCustomId('help-back')
-      .setLabel('Back')
-      .setStyle(ButtonStyle.Secondary)
-    const actionRow = new ActionRowBuilder<MessageActionRowComponentBuilder>()
-      .addComponents(button)
-
-    const fields = [
-      {
-        name: 'How to become support team member ?',
-        value: `Be active in [support server](${Options.guildInvite}) and help others to become support team member`,
-        inline: false
-      },
-      {
-        name: 'How to setup Noir for my server ?',
-        value: 'Very simple, use `settings` command and get advanced interface with all features, use buttons to navigate and setup as you want',
-        inline: false
-      }
-    ]
-
-    await Reply.reply({
-      client,
-      interaction: interaction,
-      color: Colors.primary,
-      author: 'Frequently asked questions',
-      authorImage: client.user?.avatarURL(),
-      components: [actionRow],
-      fields: fields,
-      fetch: true
     })
   }
 }
--- src/constants/Options.ts
+++ src/constants/Options.ts
@@ -8,8 +8,9 @@ export default class Options {
   public static readonly cluster = process.env.DATABASE!
   public static readonly guildId = process.env.GUILD!
   public static readonly errorChannelId = process.env.ERROR_CHANNEL_ID
-  public static readonly avatar = 'https://cdn.discordapp.com/avatars/690728155052245072/da5d06534afcc88e5cbdf50b15c6d487.png?size=4096'
   public static readonly guildInvite = process.env.INVITE!
+  public static readonly docsLink = process.env.DOCS!
+  public static readonly avatar = 'https://cdn.discordapp.com/avatars/690728155052245072/da5d06534afcc88e5cbdf50b15c6d487.png?size=4096'
   public static readonly owners = ['690728155052245072', '912298055963918416']
   public static readonly activity = 'Loid'
   public static readonly status = 'online'
